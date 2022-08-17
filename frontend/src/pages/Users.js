82eaef83fb1674f58ffe147145f889fad6f01741
omergal99 <omergal99gm@gmail.com>
2022-08-17 22:50:03 +0300

    updates

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,3 +1,8 @@
 export const Users = () => {
-  return <h2>Manage users</h2>;
+  return <div>
+    <h2>Manage users</h2>
+    <div>
+      Repo Link: <a href="https://github.com/omergal99/books-shop" target='black'>https://github.com/omergal99/books-shop</a>
+    </div>
+  </div>;
 };
